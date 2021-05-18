119b4b78065264cd8e7f47535699049f562fbfed
recchan13 <93rezao@gmail.com>
2021-05-18 17:14:46 +0700

    change email

---
+++ application/controllers/api/Otp.php
@@ -0,0 +1,16 @@
+<?php
+
+use Restserver\Libraries\REST_Controller;
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+//To Solve File REST_Controller not found
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Otp extends REST_Controller
+{
+    
+}
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -17,10 +17,38 @@ class User_model extends CI_Model
         return $this->db->get_where($this->table_name, ['email' => $email])->row_array();
     }
 
+    public function get_user_by_email_phone($email,$phone)
+    {
+        return $this->db->get_where($this->table_name, ['email' => $email, 'no_telepon' => $phone])->row_array();
+    }
+
     public function update_user_limit_remaining($nik, $limit_remaining)
     {
         $this->db->where('nik', $nik);
         $this->db->update($this->table_name, ['sisa_limit' => $limit_remaining]);
+    }
+
+    public function update_user_email($nik, $email)
+    {
+        $this->db->where('nik', $nik);
+        $this->db->update($this->table_name, ['email' => $email]);
+    }
+
+    public function update_user_pin($nik, $pin)
+    {
+        $this->db->where('nik', $nik);
+        $this->db->update($this->table_name, ['pin' => $pin]);
+    }
+
+    public function get_user_by_email_otp ($email, $otp)
+    {
+        return $this->db->get_where($this->table_name, ['email' => $email, 'otp' => $otp, 'otp_expired >=' => date('Y-m-d H:i:s') ])->row_array();
+    }
+
+    public function update_otp($email, $phone, $otp, $otp_expired)
+    {
+        $this->db->where(['email' => $email, 'no_telepon' => $phone]);
+        $this->db->update($this->table_name, ['otp' => $otp, 'otp_expired' => $otp_expired]);
     }
 
     public function get_users_pending(){
